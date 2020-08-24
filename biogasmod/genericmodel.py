302d81800b0d71ad119113fdcdd3c57eb23318e2
Joana Martins <joanamlmsantos@gmail.com>
2020-08-24 17:13:56 +0100

    style: formatting and linter
    Formatting and review of the python code using linter with flake8.

--- biogasmod/genericmodel.py
+++ biogasmod/genericmodel.py
@@ -1,64 +1,37 @@
-import pandas as pd
 from .waste import Waste
 
+
 class Model():
-    
+
     def __init__(self, q, m, l0, k, t0, t, tf):
-    
         """ Generic model class for calculating biogas
         or methane generation rate over time, which can
         be classified into zero-order, first-order,
         second-order, with multiphase or single phase.
-        
-        
+
         -----
         Atributes :
-            
+
             mass (float) representing the mass of waste in place (m),
                 unit in mass
-            methane_pot (float) representing the methane generation 
+            methane_pot (float) representing the methane generation
                 potential (l0), units of volume per mass (m3/Mg)
             gas_rate (float) first-order gas production rate constant (k)
                 in reciprocal time
-            lag_time (float)  time to methane generation start after 
+            lag_time (float)  time to methane generation start after
                 disposal (t0) (years)
             time_waste (float)  age of the waste deposited, unit in
                 time (t) (years)
             time_to_end (float): time to end-point of generation (tf)
-            data_list (a dataframe of integers and floats) a dataframe 
+            data_list (a dataframe of integers and floats) a dataframe
             of year and mass deposited extracted from the data file
         ------
 
         """
-        
+
         Waste.__init__(self, m, l0, k)
-        
+
         self.methane_gen = q
         self.lag_time = t0
         self.time_waste = t
         self.time_end = tf
-       
-    
-    def read_data_file(self, file_name):
-   
-        """Function to read in data from a csv file, using the comma delimeter. 
-       The csv file should have two columnns, the first with the years of waste 
-       deposition and the otherwith the corresponding mass deposited in each in Mg. 
-       The numbers are stored in the data attribute.
-               
-        Args:
-            file_name (string) name of a file to read from
-        
-        Returns:
-            None
-        
-        """
-            
-        data_waste = pd.read_csv(filepath_or_buffer=file_name, index_col = 0, sep=',')
-        data_waste.columns =[Year, Waste (Mg)]
-    
-        self.data = data_waste
-        
-    
-        
-       
--- biogasmod/zeroordermodel.py
+++ biogasmod/zeroordermodel.py
@@ -4,13 +4,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as pltl
 import seaborn as sns
+from .genericmodel import Model
 
-class Zero_order(Model):
-    """ Zero-order model class for calculating and 
+
+class ZeroOrder(Model):
+
+    """ Zero-order model class for calculating and
     visualizing landfill biogas and methane production
     in cubic meters per year and cumulative in the time
     to end point of generation.
-    
+
     -----
     Attributes:
         mass (float) representing the mass of waste in place,
@@ -22,75 +25,70 @@ class Zero_order(Model):
         data_list (list of floats) a list of floats of mass per year extracted
                 from the data file
     -----
-    
+
     """
-    
-    def __init__(self, q, m, l0, t0, t, tf)
-    
+
+    def __init__(self, q, m, l0, t0, t, tf):
+
         Model.__init__(self,  q, m, l0=100, t0=0.5, tf=15)
-        
-        
-    def calculate_methanegen(self, m, t)
-    
-    """ Function to calculate methane generation rate
-    in volume per time (m3/Mg). Uses a zero-order model,
-    where the rate of methane production is constant,
-    following the SWANA model:
-    
-    Q = M*L0/(t0-tf) , for t0 < t < tf
-    
-    Q = methane generation in volume per time
-    M = waste deposited, mass
-    L0 = methane generation potential, in volume per mass
-    t0 = lag time
-    tf = time to end point of generation
-    
-    
-    Reference:
-    Comparison of Models for Predicting Landfill Methane 
-    Recovery  Publication  #GR-LG  0075, The Solid Waste 
-    Association of North America (SWANA), 1998
-    
-    -----
-    Arguments:
-        None
-    
-    Returns:
-        q (dataframe) methane generation in m3 per year, for year t and waste 
-        mass deposited m
-    
-    -----
-    """
-    
-    #
-    m = self.data[Waste (Mg)]
-    t = self.data[year]
-   
-    #t[Year) = pd.date_range(tz, periods=(tf+2), freq='A')
-    
-    min_range = min(t)
-    max_range = pd.timedelta_range(min_range, periods=(tf+2), freq="A")
-    
-    #Calculates the interval of time for evaluation
-    interval = 1.0 *(max_range - min_range)
-    
-    q = pd.dataframe()
-    t = pd.Dataframe()
-    
-    #Calculates methane generation rate per year:
-    #qi = m*l0/(t0-t)
-    
-    #Calculate methane genearation rate:
-    for i in range(interval):
-        for j in m:
-            tmp = 0 + interval *i
-            t.append(tmp)
-            q.append(j*l0/(t0-tmp))
-    
-    #Merge dataframes
-    q = pd.concate([t, q] , axis=1)              
-    
-    return q
+
+    def calculate_methanegen(self, m, t):
+        """ Function to calculate methane generation rate
+        in volume per time (m3/Mg). Uses a zero-order model,
+        where the rate of methane production is constant,
+        following the SWANA model:
+
+        Q = M*L0/(t0-tf) , for t0 < t < tf
+
+        Q = methane generation in volume per time
+        M = waste deposited, mass
+        L0 = methane generation potential, in volume per mass
+        t0 = lag time
+        tf = time to end point of generation
 
 
-        
+        Reference:
+        Comparison of Models for Predicting Landfill Methane
+        Recovery  Publication  #GR-LG  0075, The Solid Waste
+        Association of North America (SWANA), 1998
+
+        -----
+        Args:
+            None
+
+        Returns:
+            q (dataframe) methane generation in m3 per year, for year t
+            and waste mass deposited m
+
+        -----
+        """
+
+        #
+        m = self.data["Waste(Mg)"]
+        t = self.data["Year"]
+
+        # t[Year) = pd.date_range(tz, periods=(tf+2), freq='A')
+
+        min_range = min(t)
+        max_range = pd.timedelta_range(min_range, periods=(tf + 2), freq="A")
+
+        # Calculates the interval of time for evaluation
+        interval = 1.0 * (max_range - min_range)
+
+        q = pd.dataframe()
+        t = pd.Dataframe()
+
+        # Calculates methane generation rate per year:
+        # qi = m*l0/(t0-t)
+
+        # Calculate methane genearation rate:
+        for i in range(interval):
+            for j in m:
+                tmp = 0 + interval * i
+                t.append(tmp)
+                q.append(j * l0 / (t0 - tmp))
+
+        # Merge dataframes
+        q = pd.concat([t, q], axis=1)
+
+        return q
